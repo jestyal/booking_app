9d0d17abb7d6f7f2041f9446ebbd8dc3e4a728ac
jestyal <natali.doroshenko14@gmail.com>
2022-09-14 09:43:51 +0500

    init

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Tickets"),
+      ),
+      body: const Center(child: Text("Body")),
+    );
+  }
+}
