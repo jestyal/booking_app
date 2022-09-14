bd297d8b1a2491d9734026c50951dbcea0f3aa3e
jestyal <natali.doroshenko14@gmail.com>
2022-09-14 10:57:12 +0500

    Added home screen. Added constant colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Color background = const Color.fromRGBO(255, 255, 255, 1);
+// Color activeButton = const Color.fromRGBO(102, 153, 204, 1);
+// Color basicText = const Color.fromARGB(177, 0, 0, 0);
+Color iconColor = const Color(0xFF526480);
--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,6 +1,8 @@
-import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import '/screens/home_screen.dart';
+import '/constants/colors.dart';
 
 class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
@@ -10,19 +12,36 @@ class BottomBar extends StatefulWidget {
 }
 
 class _BottomBarState extends State<BottomBar> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const HomeScreen(),
+    const Text("Search"),
+    const Text("Ticket"),
+    const Text("Profile"),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Tickets"),
       ),
-      body: const Center(child: Text("Body")),
+      body: Center(child: _widgetOptions[_selectedIndex]),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
         elevation: 10,
         showUnselectedLabels: false,
         showSelectedLabels: false,
-        selectedItemColor: Colors.blueGrey,
-        unselectedItemColor: const Color(0xFF526480),
+        selectedItemColor: iconColor,
+        unselectedItemColor: iconColor,
+        type: BottomNavigationBarType.fixed,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
@@ -41,7 +60,7 @@ class _BottomBarState extends State<BottomBar> {
           ),
           BottomNavigationBarItem(
             icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
-            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_add_filled),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
             label: "Profile",
           ),
         ],
