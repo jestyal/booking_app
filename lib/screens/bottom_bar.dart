90f19a9054e28b4d4c1f652466dc885f1243e534
jestyal <natali.doroshenko14@gmail.com>
2022-09-14 10:17:47 +0500

    Added bottom bar icons

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +17,35 @@ class _BottomBarState extends State<BottomBar> {
         title: const Text("Tickets"),
       ),
       body: const Center(child: Text("Body")),
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 10,
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526480),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+            label: "Ticket",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_add_filled),
+            label: "Profile",
+          ),
+        ],
+      ),
     );
   }
 }
