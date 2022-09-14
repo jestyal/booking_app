466515e2f2b2c79ee461560bc28f793a684dce9c
jestyal <natali.doroshenko14@gmail.com>
2022-09-14 13:56:52 +0500

    Added ticket icon (it`s widget)

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -2,6 +2,7 @@ import 'package:booking_app/utils/app_layout.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../utils/app_styles.dart';
+import '../widgets/ticket_container.dart';
 
 class TicketView extends StatelessWidget {
   const TicketView({Key? key}) : super(key: key);
@@ -18,10 +19,7 @@ class TicketView extends StatelessWidget {
           children: [
             Container(
               decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20)
-                ),
+                borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
                 color: Styles.blueColor,
               ),
               padding: const EdgeInsets.all(16),
@@ -30,6 +28,36 @@ class TicketView extends StatelessWidget {
                   Row(
                     children: [
                       Text("NYC", style: Styles.headLineStyle3.copyWith(color: Styles.whiteColor)),
+                      const Spacer(),
+                      const TicketContainer(),
+                      Expanded(child: Stack(
+                        children: [
+                          SizedBox(
+                            height: 24,
+                            child: LayoutBuilder(
+                              builder: (BuildContext context, BoxConstraints constraints) {
+                                return Flex(
+                                  direction: Axis.horizontal,
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: List.generate(
+                                      (constraints.constrainWidth() / 6).floor(),
+                                      (index) => SizedBox(
+                                            width: 3,
+                                            height: 1,
+                                            child: DecoratedBox(decoration: BoxDecoration(color: Styles.whiteColor)),
+                                          )),
+                                );
+                              },
+                            ),
+                          ),
+                          Center(
+                            child: Transform.rotate(
+                                angle: 1.5, child: Icon(Icons.local_airport_rounded, color: Styles.whiteColor)),
+                          ),
+                        ],
+                      )),
+                      const TicketContainer(),
                       const Spacer(),
                       Text("LDN", style: Styles.headLineStyle3.copyWith(color: Styles.whiteColor)),
                     ],
---
+++ lib/widgets/ticket_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '/utils/app_styles.dart';
+
+class TicketContainer extends StatelessWidget {
+  const TicketContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(3.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(width: 2.5, color: Styles.whiteColor),
+      ),
+    );
+  }
+}
